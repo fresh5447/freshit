b952670ab6c2d33c5cdd0aa22ee247b59319341e
vagrant <vagrant@rails-dev-box>
2014-03-06 21:29:51 +0000

    Created Popular Posts and Users, and optimized topics#show with eager loading.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.top_rated.paginate(page: params[:page], per_page: 10)
+  end
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.visible_to(current_user)
